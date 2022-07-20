74c36a7c8d2366d718c4e1c5007e1f5a1259dc68
turkish joe <turkishjoe283@gmail.com>
2022-07-20 22:56:39 +0300

    update

--- 2/view/menu.php
+++ 2/view/menu.php
@@ -2,5 +2,6 @@
 <div class="wrap_menu">
     <span>Меню и заголовок</span>
 
-    <a href=""></a>
+    <a href="?block=list_product">Список товаров</a>
+    <a href="?block=signle_product">Список новостей</a>
 </div>
