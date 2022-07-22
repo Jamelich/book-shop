41819d0ab9c5396fd8a6d6df623af07915401793
Jamelich <serj-post@mail.ru>
2022-07-22 17:03:32 +0300

    Переписал все в 1 файл

--- footer.php
+++
@@ -1,8 +0,0 @@
-<div class="wrap_footer">
-    <span>Подвал</span>
-</div>
-
-
-</body>
-
-</html>
--- functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $dbh = new PDO("mysql:host=localhost;dbname=shop-book", 'root', '');
-    $stmt = $dbh->query("SELECT * FROM `products`");
-    // $results = $stmt->fetch(PDO::FETCH_ASSOC);
-    $array_products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    die($e->getMessage());
-}
